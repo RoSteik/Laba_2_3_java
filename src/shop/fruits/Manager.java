343d21026b9aefc4ea14505f15f1d85ce9ab8acb
RoSteik <rostyslav.postolaki@gmail.com>
2022-04-02 18:55:09 +0300

    add laba 3 code

---
+++ src/shop/fruits/Manager.java
@@ -0,0 +1,38 @@
+package shop.fruits;
+
+import lombok.Getter;
+
+import java.sql.SQLOutput;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class Manager{
+    public List<FruitBox> sortSummerFruitsByColour(List<FruitBox> fruitBoxes, boolean isDescendingOrderNeeded){
+        if(isDescendingOrderNeeded == true){
+            return fruitBoxes.stream()
+                    .filter(fruit -> fruit.getRipeningSeason() == "Summer")
+                    .sorted(Comparator.comparing(FruitBox::getColourOfFruitInBox).reversed())
+                    .collect(Collectors.toList());
+        }
+        return fruitBoxes.stream()
+                .filter(fruit -> fruit.getRipeningSeason() == "Summer")
+                .sorted(Comparator.comparing(FruitBox::getColourOfFruitInBox))
+                .collect(Collectors.toList());
+    }
+    public List<FruitBox> sortSummerFruitsByPrice(List<FruitBox> fruitBoxes, boolean isDescendingOrderNeeded){
+        if(isDescendingOrderNeeded == true){
+            return fruitBoxes.stream()
+                    .filter(fruit -> fruit.getRipeningSeason() == "Summer")
+                    .sorted(Comparator.comparing(FruitBox::getPrice).reversed())
+                    .collect(Collectors.toList());
+        }
+        return fruitBoxes.stream()
+                .filter(fruit -> fruit.getRipeningSeason() == "Summer")
+                .sorted(Comparator.comparing(FruitBox::getPrice))
+                .collect(Collectors.toList());
+
+    }
+
+}
